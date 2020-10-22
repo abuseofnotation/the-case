04f2662e8252fb052f4fbce6c41cc313fa3c513b
Boris Marinov <boris.marinov@xogito.com>
2020-10-22 20:18:40 +0300

    stuff

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pandoc title.txt 0*.md -o the-case.epub
 cat 0*.md | sed 's/\[.*)//g' > the-case.txt
-
-
+cat 0*.md | sed 's/\[.*)//g' > the-case.md
+pandoc title.txt the-case.md -o the-case.epub
+pandoc title.txt the-case.md -o the-case.docx
